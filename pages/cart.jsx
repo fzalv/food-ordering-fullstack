e7c7ffb2d9a660e0984bdd19400e6423d275bb29
haris <kumantheas@gmail.com>
2022-06-12 11:27:29 +0700

    Food Order Cash
    Paymen cash COD

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
-import Image from "next/image";
 import styles from "../styles/Cart.module.css";
+import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   PayPalScriptProvider,
   PayPalButtons,
-  usePayPalScriptReducer
+  usePayPalScriptReducer,
 } from "@paypal/react-paypal-js";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { reset } from "../redux/cartSlice";
+import OrderDetail from "../components/OrderDetail";
 
 const Cart = () => {
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector((state) => state.cart);
   const [open, setOpen] = useState(false);
+  const [cash, setCash] = useState(false);
   const amount = cart.total;
   const currency = "USD";
-  const style = { "layout": "vertical" };
+  const style = { layout: "vertical" };
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -48,46 +50,46 @@ const Cart = () => {
       });
     }, [currency, showSpinner]);
 
-
-    return (<>
-      {(showSpinner && isPending) && <div className="spinner" />}
-      <PayPalButtons
-        style={style}
-        disabled={false}
-        forceReRender={[amount, currency, style]}
-        fundingSource={undefined}
-        createOrder={(data, actions) => {
-          return actions.order
-            .create({
-              purchase_units: [
-                {
-                  amount: {
-                    currency_code: currency,
-                    value: amount,
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
                   },
-                },
-              ],
-            })
-            .then((orderId) => {
-              // Your code here after create the order
-              return orderId;
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId;
+              });
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function (details) {
+              const shipping = details.purchase_units[0].shipping;
+              createOrder({
+                customer: shipping.name.full_name,
+                address: shipping.address.address_line_1,
+                total: cart.total,
+                method: 1,
+              });
             });
-        }}
-        onApprove={function (data, actions) {
-          return actions.order.capture().then(function (details) {
-            const shipping = details.purchase_units[0].shipping;
-            createOrder({
-              customer: shipping.name.full_name,
-              address: shipping.address.address_line_1,
-              total: cart.total,
-              method: 1,
-            });
-          });
-        }}
-      />
-    </>
+          }}
+        />
+      </>
     );
-  }
+  };
 
   return (
     <div className={styles.container}>
@@ -104,8 +106,7 @@ const Cart = () => {
             </tr>
           </tbody>
           <tbody>
-
-            {cart.products.map(product => (
+            {cart.products.map((product) => (
               <tr className={styles.tr} key={product._id}>
                 <td>
                   <div className={styles.imgContainer}>
@@ -122,7 +123,7 @@ const Cart = () => {
                 </td>
                 <td>
                   <span className={styles.extras}>
-                    {product.extras.map(extra => (
+                    {product.extras.map((extra) => (
                       <span key={extra._id}>{extra.text}, </span>
                     ))}
                   </span>
@@ -134,7 +135,9 @@ const Cart = () => {
                   <span className={styles.quantity}>{product.quantity}</span>
                 </td>
                 <td>
-                  <span className={styles.total}>${product.price * product.quantity}</span>
+                  <span className={styles.total}>
+                    ${product.price * product.quantity}
+                  </span>
                 </td>
               </tr>
             ))}
@@ -143,54 +146,44 @@ const Cart = () => {
       </div>
       <div className={styles.right}>
         <div className={styles.wrapper}>
-          <h2 className={styles.title}>Cart Total</h2>
+          <h2 className={styles.title}>CART TOTAL</h2>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>
-              Subtotal:
-            </b>
-            ${cart.total}
+            <b className={styles.totalTextTitle}>Subtotal:</b>${cart.total}
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>
-              Discount:
-            </b>
-            $0.00
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
           </div>
           <div className={styles.totalText}>
-            <b className={styles.totalTextTitle}>Total:</b>
-            ${cart.total}
+            <b className={styles.totalTextTitle}>Total:</b>${cart.total}
           </div>
           {open ? (
             <div className={styles.paymentMethods}>
               <button
                 className={styles.payButton}
+                onClick={() => setCash(true)}
               >
-                {/* onClick={() => setCash(true)} */}
                 CASH ON DELIVERY
               </button>
               <PayPalScriptProvider
                 options={{
-                  "client-id": "AZ-oxGgB8WeSZA-YWXx0YzL9n553nG8GGg80NSHBRvGbrLevXaWc2cZU4I_LlWrYtVTzuZPtsadDEa2u",
+                  "client-id":
+                    "AZ-oxGgB8WeSZA-YWXx0YzL9n553nG8GGg80NSHBRvGbrLevXaWc2cZU4I_LlWrYtVTzuZPtsadDEa2u",
                   components: "buttons",
                   currency: "USD",
                   "disable-funding": "credit,card,p24",
-
                 }}
               >
-                <ButtonWrapper
-                  currency={currency}
-                  showSpinner={false}
-                />
+                <ButtonWrapper currency={currency} showSpinner={false} />
               </PayPalScriptProvider>
             </div>
           ) : (
-
             <button onClick={() => setOpen(true)} className={styles.button}>
               CHECKOUT NOW!
             </button>
           )}
         </div>
       </div>
+      {cash && <OrderDetail total={cart.total} createOrder={createOrder} />}
     </div>
   );
 };
